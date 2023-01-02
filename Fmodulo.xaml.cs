4510f73e9aec179c4d4e6d86912e9e8979625aec
Enstso Janvier <enstso@outlook.fr>
2023-01-02 18:02:16 +0100

    Création des views des algos de chiffrement et Méthode générer mot de passe

--- Fmodulo.xaml.cs
+++ Fmodulo.xaml.cs
@@ -23,5 +23,7 @@ namespace encrytionapp
         {
             InitializeComponent();
         }
+
+        
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,24 +31,42 @@ namespace encrytionapp
 
         private void BtnVigenre_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Fvigenere fvigenere = new Fvigenere();
+            fvigenere.Show();
         }
 
         private void BtnRot13_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            
+            Frot13 frot13 = new Frot13();
+            frot13.Show();
         }
 
         private void BtnModulo_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Fmodulo modulo = new Fmodulo();
+            modulo.Show();
         }
 
         private void BtnGenererunmotpasse_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Le mot de passe généré \n");
+            MessageBox.Show("Le mot de passe généré : \n"+GenererMotDePasse());
         }
 
+        private string GenererMotDePasse()
+        {
+            string mdp = "";
+            char[] tab = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X','Y', 'Z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','1','2','3','4','5','6','7','8','9','@',':','.','_','-','#','&','!','$','*'};
+            Random aléatoire = new Random();
+            Console.WriteLine(tab.Length);
+            int choice = 0;
+            for (int i = 0; i < 12; i++)
+            {
+               choice = aléatoire.Next(0, tab.Length);
+               mdp = mdp + Convert.ToString(tab[choice]); 
+            }
+            return mdp;
+        }
         
     }
 }
